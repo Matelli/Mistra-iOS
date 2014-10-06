44414522954a0fc0a58b83a52ad8b0a36544b6ac
Jonathan Schmidt <Jonathan.schmidt@matelli.fr>
2014-10-06 12:34:26 +0200

    Changed #import to @import to correct warning

--- Mistra FormationTests/Mistra_FormationTests.m
+++ Mistra FormationTests/Mistra_FormationTests.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Mistra. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+@import XCTest;
 
 @interface Mistra_FormationTests : XCTestCase
 
@@ -24,11 +24,6 @@
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
 @end
